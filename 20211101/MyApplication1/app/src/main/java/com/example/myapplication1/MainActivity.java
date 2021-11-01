efb9f5148c988e231f719c0aa7778b1cec43b822
7flyx <2647282717@qq.com>
2021-11-01 22:00:18 +0800

    登录页面

---
+++ 20211101/MyApplication1/app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myapplication1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"登录成功",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, LoginSuccessActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+}
